022098427246e97224175119eebe574f534928ba
Apex4598 <login2ajit@gmail.com>
2022-08-13 06:50:57 +0530

    Welcome to Address Book program

---
+++ src/com/bridgelabz/AddressBook2/AddressMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.AddressBook2;
+
+public class AddressMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program Management ");
+    }
+
+}
